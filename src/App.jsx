6db0f923670c4ccf11d712cf4f94ff702ef50123
Seneshperera <seneshperera2000@gmail.com>
2025-11-22 16:31:08 +0530

    remove index

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import { Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import SkillsSection from './components/SkillsSection';
+import GraphicDesignSection from './components/GraphicDesignSection';
+import ProjectsSection from './components/ProjectsSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Index />} />
+    <Route path="/" element={
+      <div className="min-h-screen text-foreground relative">
+        {/* Animated Background */}
+        <div className="fixed inset-0 z-0">
+          <div className="floating-dots absolute top-10 left-10"></div>
+          <div className="floating-dots absolute top-20 right-20" style={{ animationDelay: '2s' }}></div>
+          <div className="floating-dots absolute bottom-20 left-1/4" style={{ animationDelay: '4s' }}></div>
+          <div className="floating-dots absolute top-1/3 right-1/3" style={{ animationDelay: '6s' }}></div>
+          <div className="floating-dots absolute bottom-10 right-10" style={{ animationDelay: '8s' }}></div>
+        </div>
+        <NavBar />
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <GraphicDesignSection />
+        <ProjectsSection />
+        <TestimonialsSection />
+        <ContactSection />
+        <Footer />
+        <ScrollToTop />
+      </div>
+    } />
   </Routes>
 );
 
